64cbe10d70855d0bf9bafd156ba3d8246fd0d71f
spencer-rafada <spencer.byui@gmail.com>
2023-04-26 07:20:19 -0600

    component basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningComponent, SuccessComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
