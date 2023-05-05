217d08eb865a6755ee8166a11650eb5222359bba
spencer-rafada <spencer.byui@gmail.com>
2023-05-05 10:14:57 -0600

    assigning alias to custom events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,11 +6,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent {
-  @Output() serverCreated = new EventEmitter<{
+  @Output('srvCreated') serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
-  @Output() blueprintCreated = new EventEmitter<{
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
