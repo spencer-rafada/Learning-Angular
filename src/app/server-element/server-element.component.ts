840442d4ef205395680f840b5ad8fdbd34fc4cfb
spencer-rafada <spencer.byui@gmail.com>
2023-05-05 10:45:54 -0600

    Seeing Lifecycles in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewEncapsulation,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,10 +21,55 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   // encapsulation: ViewEncapsulation.None,
   // encapsulation: ViewEncapsulation.Native, shadow DOM technology
 })
-export class ServerElementComponent {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy
+{
   @Input('serverElement') element: {
     type: string;
     name: string;
     content: string;
   };
+
+  constructor() {
+    console.log(`constructor called`);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(`onChanges called`);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log(`doChange called`);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(`ngContentAfter Init called`);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(`ngContentAfterChecked called`);
+  }
+  ngAfterViewInit(): void {
+    console.log(`ngAfterViewInit called`);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(`ngAfterViewChecked called`);
+  }
+
+  ngOnDestroy(): void {
+    console.log(`ngDestroy`);
+  }
 }
