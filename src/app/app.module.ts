681aa988067edc58300565097c7c5493c0310895
spencer-rafada <spencer.byui@gmail.com>
2023-05-22 23:11:13 -0600

    routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './account.service';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, AccountComponent, NewAccountComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [AccountsService],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [ServersService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/logging.service.ts
+++
@@ -1,5 +0,0 @@
-export class LoggingService {
-  logStatus(status: string) {
-    console.log('A server status changed, new status ' + status);
-  }
-}
