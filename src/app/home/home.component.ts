a5dc1fa29dfa071be10175963fac63463b3046a9
spencer-rafada <spencer.byui@gmail.com>
2023-05-31 22:20:05 -0600

    finished observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,46 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+        count++;
+      }, 1000);
     });
+
+    this.firstObsSubscription = customIntervalObservable
+      // can take multiple arguments to manipulate data
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data) => {
+          return `Round ` + data;
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error);
+        },
+        () => {
+          console.log('complete');
+        }
+      );
   }
 
   // prevent memory leak
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id: number;
+  private activatedSub: Subscription;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
+    this.activatedSub = this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    // this.userService.activatedEmitter.emit(true);
+    this.userService.activatedEmitter.next(true);
+  }
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
   }
 }
